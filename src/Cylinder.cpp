543c73a3cded67e876c08bccb400097b1eb4ce63
nora950817 <nora.ee13@nycu.edu.tw>
2025-04-21 19:33:58 +0800

    Cylinder.cpp

--- src/Cylinder.cpp
+++ src/Cylinder.cpp
@@ -1,33 +1,32 @@
-# ifndef CYLINDER_CPP
-# define CYLINDER_CPP
+#ifndef CYLINDER_CPP
+#define CYLINDER_CPP
 
-# include "Cylinder.h"
+#include "Cylinder.h"
+#include <iomanip>  
 
-double Cylinder :: SurfaceArea(){
-    
-    return ;
+double Cylinder::SurfaceArea() {
+    return 2 * 3.1415 * radius * (radius + height);
 }
 
-double Cylinder :: Volume(){
-
-    return ;
+double Cylinder::Volume() {
+    return 3.1415 * radius * radius * height;
 }
 
-double Cylinder :: Circumference(){
-
-    return ;
+double Cylinder::Circumference() {
+    return 4 * 3.1415 * radius;
 }
 
-istream & operator>>(istream & in, Cylinder & cldr)
-{
-
-    return ;
+istream& operator>>(istream& in, Cylinder& cldr) {
+    in >> cldr.radius >> cldr.height;
+    return in;
 }
 
-ostream & operator<<(ostream & out, Cylinder & cldr)
-{
-    
-    return ;
+ostream& operator<<(ostream& out, Cylinder& cldr) {
+    out << fixed << setprecision(3);
+    out << "Circumference: " << cldr.Circumference() << endl;
+    out << "SurfaceArea: " << cldr.SurfaceArea() << endl;
+    out << "Volume: " << cldr.Volume();
+    return out;
 }
 
-# endif
+#endif
